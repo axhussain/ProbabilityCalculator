8bd67d3e62150e610a31b3ff21dc992313a56f0c
Adnan Hussain <axhussain@gmail.com>
2023-04-10 11:00:45 +0100

    Add first failing test

--- ProbCalc.Domain.Tests/UnitTest1.cs
+++ ProbCalc.Domain.Tests/UnitTest1.cs
@@ -5,7 +5,29 @@ namespace ProbCalc.Domain
         [Fact]
         public void Test1()
         {
+            //Arrange
+            var p1 = 0.5f;
+            var p2 = 0.5f;
+            var sut = new CalculatorService();
 
+            //Act
+            var actual = sut.CombinedWith(p1, p2);
+
+            //Assert
+            var expected = 0.25f;
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class CalculatorService
+    {
+        public CalculatorService()
+        {
+        }
+
+        public float CombinedWith(float p1, float p2)
+        {
+            return default;
         }
     }
 }
